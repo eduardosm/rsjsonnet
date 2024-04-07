8095ca3c0a9619551e4de00c220c9bebaca72a50
Eduardo Sánchez Muñoz <eduardosm-dev@e64.io>
2024-04-07 18:46:16 +0200

    ci: create crate packages

---
+++ ci/package-crates.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+pkgs_dir="packages"
+if [ -e "$pkgs_dir" ]; then
+  echo "$pkgs_dir already exists"
+  exit 1
+fi
+
+version="$(awk '/^version = ".+"$/ { sub("^version = \"", ""); sub("\"$", ""); print }' Cargo.toml)"
+
+echo "::group::Fetch dependencies"
+cargo fetch --locked
+echo "::endgroup::"
+
+echo "::group::Vendor dependencies"
+mkdir .cargo
+cargo vendor --frozen "$pkgs_dir" > .cargo/config.toml
+echo "::endgroup::"
+
+crates=(
+    rsjsonnet-lang
+    rsjsonnet-front
+    rsjsonnet
+)
+
+for crate in "${crates[@]}"; do
+  echo "::group::Package $crate"
+  cargo package -p "$crate" --no-verify --frozen
+  tar -xf "target/package/$crate-$version.crate" -C "$pkgs_dir"
+  pkg_checksum="$(sha256sum "target/package/$crate-$version.crate" | awk '{print $1}')"
+  echo "{\"files\":{},\"package\":\"$pkg_checksum\"}" > "$pkgs_dir/$crate-$version/.cargo-checksum.json"
+  echo "::endgroup::"
+done
+
+mkdir output
+for crate in "${crates[@]}"; do
+  cp -t output "target/package/$crate-$version.crate"
+done
