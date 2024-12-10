de514e87c6f4e720bd157f5ce734f43bad9e338b
Eduardo Sánchez Muñoz <eduardosm-dev@e64.io>
2024-12-10 13:29:02 +0100

    test(ui): remove unused `not-test` parameter

--- rsjsonnet/tests/ui/defs.rs
+++ rsjsonnet/tests/ui/defs.rs
@@ -1,8 +1,6 @@
 #[derive(Debug, serde::Deserialize)]
 #[serde(deny_unknown_fields)]
 pub(crate) struct TestParams {
-    #[serde(rename = "not-test", default = "false_")]
-    pub(crate) not_test: bool,
     #[serde(default = "Vec::new")]
     pub(crate) args: Vec<String>,
     #[serde(rename = "no-color", default = "true_")]
@@ -14,17 +12,11 @@ pub(crate) struct TestParams {
 impl Default for TestParams {
     fn default() -> Self {
         Self {
-            not_test: false,
             args: Vec::new(),
             no_color: true,
             exit_code: None,
         }
     }
-}
-
-#[inline]
-fn false_() -> bool {
-    false
 }
 
 #[inline]
